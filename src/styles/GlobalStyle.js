ec00ee01205ea047468a1ec5d6e1689c20c7e4ce
hyunmin200 <gyejeongjin@gmail.com>
2024-06-25 21:50:27 +0900

    init

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+* {
+  box-sizing: border-box;
+}
+html {
+  // 1rem = 10px
+  font-size:62.5%;
+}
+body{
+  width: 100vw;
+  height: 100vh;
+  color: white;
+}
+#root {
+  width: 100%;
+  height: 100%;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+  cursor: pointer;
+}
+input, button {
+  background-color: transparent;
+  border: none;
+  outline: none;
+}
+h1, h2, h3, h4, h5, h6{
+  
+}
+ol, ul, li{
+  list-style: none;
+}
+img {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+`;
+
+export default GlobalStyle;
